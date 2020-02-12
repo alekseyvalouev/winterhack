e798d70723a42bc5315ac5cc2adfdba6bc98a04d
Tonylai2789 <55564705+Tonylai2789@users.noreply.github.com>
2020-02-12 14:25:48 -0800

    Add files via upload
    Randomized it, have logic error and need to loop + put in class

---
+++ Winterhack.py
@@ -0,0 +1,19 @@
+import pygame
+import random as r
+x = 1920  #dimensions for display surface object
+y = 1080
+x1 = r.randint(0,1700) #randomize value for snowflake drop   
+pygame.init()
+screen = pygame.display.set_mode([1920,1080])
+white = [0, 0, 0]
+screen.fill(white)
+number_of_snowflakes = 0
+pygame.display.set_caption('Image')
+display_surface = pygame.display.set_mode((x,y))
+# create a surface object, image is drawn on it. 
+snowflake2 = pygame.image.load(r'C:\Users\logit\Downloads\snowflake2.png')
+display_surface.fill(white)
+#copying image surface object
+display_surface.blit(snowflake2, (x1, 0)) #Y extreme is -260
+pygame.display.update()
+    
