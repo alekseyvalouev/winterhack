5c89fbd5c2981a69d006787c2f89d3a91e5e6d99
RampageRager <60833517+RampageRager@users.noreply.github.com>
2020-02-12 21:49:01 -0800

    Add files via upload
    now with moving snowflakes etc

--- Winterhack.py
+++ Winterhack.py
@@ -1,21 +1,62 @@
 import pygame
 import random as r
-x = 1920  #dimensions for display surface object
-y = 1080
-x1 = r.randint(0,1700) #randomize value for snowflake drop   
-pygame.init()
-screen = pygame.display.set_mode([1920,1080])
-white = [0, 0, 0]
-screen.fill(white)
+
+#snowflake array
+snowflakes = []
+
+#movement speed
+dx = 0
+dy = 3
+
+#dimensions for display surface object
+x = 1080
+y = 720
+
+#randomize initial placing for first snowflake
+x1 = r.randint(0,980)
+
+#chooses which image of snowflake to use
+picchooser = r.randint(0,3)
+
+#loop placeholder
 abc = 1
-number_of_snowflakes = 0
+
+#bg colour
+black = [0, 0, 0]
+
+#start pygame and set up canvas
+pygame.init()
+screen = pygame.display.set_mode([1080,720])
+screen.fill(black)
 pygame.display.set_caption('Image')
 display_surface = pygame.display.set_mode((x,y))
+
+#define images and scale them
+snowflake2 = pygame.image.load('snowflake2.png')
+snowflake1 = pygame.image.load('snowflake1.png')
+snowflake2_c = pygame.transform.scale(snowflake2, (7, 7))
+snowflake1_c = pygame.transform.scale(snowflake1, (7, 7))
+
+#make bg black
+display_surface.fill(black)
+
+for i in range(1000):
+    picchooser = r.randint(1,2)
+    snowflakes.append([r.randint(0, 1080), r.randint(0,720), picchooser])
+
+#perpetual loop
 while (abc == 1):
-    # create a surface object, image is drawn on it. 
-    snowflake2 = pygame.image.load('snowflake2.png')
-    display_surface.fill(white)
-    #copying image surface object
-    display_surface.blit(snowflake2, (x1, 0)) #Y extreme is -260
+
+    #clear background for next snowflake
+    display_surface.fill(black)
+
+    #determines which image to use
+    for q in snowflakes:
+        q[1] += dy
+        if (q[2] == 1):
+            display_surface.blit(snowflake1_c, (q[0], q[1]))
+        elif (q[2] == 1):
+            display_surface.blit(snowflake2_c, (q[0], q[1]))
+
+    #updates
     pygame.display.update()
-        
