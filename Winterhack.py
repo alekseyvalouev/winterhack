9d5fcbe9cc89fa9b6e077093990ab1707212ab13
RampageRager <60833517+RampageRager@users.noreply.github.com>
2020-02-12 20:21:16 -0800

    Add files via upload
    updated winterhack - now working

--- Winterhack.py
+++ Winterhack.py
@@ -7,13 +7,15 @@ pygame.init()
 screen = pygame.display.set_mode([1920,1080])
 white = [0, 0, 0]
 screen.fill(white)
+abc = 1
 number_of_snowflakes = 0
 pygame.display.set_caption('Image')
 display_surface = pygame.display.set_mode((x,y))
-# create a surface object, image is drawn on it. 
-snowflake2 = pygame.image.load(r'C:\Users\logit\Downloads\snowflake2.png')
-display_surface.fill(white)
-#copying image surface object
-display_surface.blit(snowflake2, (x1, 0)) #Y extreme is -260
-pygame.display.update()
-    
+while (abc == 1):
+    # create a surface object, image is drawn on it. 
+    snowflake2 = pygame.image.load('snowflake2.png')
+    display_surface.fill(white)
+    #copying image surface object
+    display_surface.blit(snowflake2, (x1, 0)) #Y extreme is -260
+    pygame.display.update()
+        
